c8768bbeb3818a33a357cd20924cae6ef73213fd
Derrick Guerrero <derrick.guerrero@aviture.us.com>
2024-02-11 19:05:29 -0600

    Default to /home-app basePath.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.basePath,
-    // '/home-app',
+    basePath: process.env.basePath ?? '/home-app',
     output: 'export',
 };
 
